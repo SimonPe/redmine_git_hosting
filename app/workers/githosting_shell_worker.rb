ea7ca8c95f132ec2a076b9bd713bad9a20e28c46
Simon Peeters <simon@inuits.eu>
2016-05-06 01:06:46 +0200

    don't add duplicate jobs

--- app/workers/githosting_shell_worker.rb
+++ app/workers/githosting_shell_worker.rb
@@ -3,6 +3,14 @@ class GithostingShellWorker
 
   sidekiq_options queue: :redmine_git_hosting, retry: false
 
+  def self.maybe_do(command, object, options = {})
+    Sidekiq::Queue.new(:redmine_git_hosting).each |job|
+      return nil if job.args == [command, object, options]
+    end
+
+    perform_async(command, object, options)
+  end
+
   def perform(command, object, options = {})
     logger.info("#{command} | #{object} | #{options}")
     RedmineGitHosting::GitoliteWrapper.resync_gitolite(command, object, options)
